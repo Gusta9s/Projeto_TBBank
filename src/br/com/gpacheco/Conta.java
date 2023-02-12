4ee42dcce949a89c14db75f42dd4b8557cbe4ab9
Gustavo de Almeida Pacheco001 <gustavoapacheco@yahoo.com.br>
2023-02-11 23:26:47 -0300

    1.1

--- src/br/com/gpacheco/Conta.java
+++ src/br/com/gpacheco/Conta.java
@@ -1,34 +1,34 @@
 package br.com.gpacheco;
 
-public class Conta {
-	double saldo;
+public class Conta {                                  // Criando o objeto Conta...
+	double saldo;                                          // Declarando o atributo saldo.
 	
-	public boolean deposita(double valor) {
-		if(this.saldo >= valor) {
+	public boolean deposita(double valor) {                       // Passando o parametro "valor" do tipo Double para identificar o tipo de parametro para classe principal, o booleano é para dar o retorno de sucesso ou falha!. 
+		if(this.saldo >= valor) {                       // Condição para ser efetuado o deposito.
 			saldo += valor;
-			return true;
+			return true;                            // Retorna para classe principal a informação que deu tudo certo!.
 		}
 		
 		else {
-			return false;
+			return false;                           // Retorna para classe principal a informação de que a operação foi cancelada!.
 		}
 	}
 	
-	public boolean saca(double valor) {
-		if(this.saldo >= valor) {
+	public boolean saca(double valor) {                     // Passando o parametro "valor" do tipo Double para identificar o tipo de parametro para classe principal, o booleano é para dar o retorno de sucesso ou falha!
+		if(this.saldo >= valor) {                       // Condição para ser efetuado o saque.
 			this.saldo -= valor;
-			return true;
+			return true;                                        // Retorna para classe principal a informação que deu tudo certo!.
 		} 
 		
 		else {
-			return false;
+			return false;                                      // Retorna para classe principal a informação de que a operação foi cancelada!.
 		}
 	}
 	
-	public boolean transfere(double valor, Conta destino) {
-		if(this.saldo >= valor) {
-			this.saca(valor);
-			destino.deposita(valor);
+	public boolean transfere(double valor, Conta destino) {       // Passando os parametros valor e conta para classe principal, e o retorno delas é booleano.
+		if(this.saldo >= valor) {                                       // Condição para ser efetuada a transferencia.
+			this.saca(valor);                                               // Me utilizando de metodos prontos para simplificar meu codigo, pois a operação é igual.
+			destino.deposita(valor);                                           // Me utilizando de metodos prontos para simplificar meu codigo, pois a operação é igual e destino é a conta destino do deposito.
 			return true;
 		}
 		else {
--- src/br/com/gpacheco/Referencias_Java.java
+++ src/br/com/gpacheco/Referencias_Java.java
@@ -3,14 +3,14 @@ package br.com.gpacheco;
 public class Referencias_Java {
 	public static void main(String args[]) {
 		
-		Conta primeiraconta = new Conta();
+		Conta primeiraconta = new Conta();                  // Instanciando uma nova referencia.
 		Conta segundaconta = new Conta();
 		
-		primeiraconta.saldo = 400;
+		primeiraconta.saldo = 400;                // Inserindo valores na referencia.
 		segundaconta.saldo = 300;
 		
-		boolean valorParaDeposito = primeiraconta.deposita(50);
-		if(valorParaDeposito) {
+		boolean valorParaDeposito = primeiraconta.deposita(50);        // Operando o deposito e criando uma condição booleana.
+		if(valorParaDeposito) {                                                              // Por ser um valor booleano, ele vai permitir fazer a estrutura condicional.  
 			System.out.println("Deposito realizado com sucesso!.");
 			System.out.println(primeiraconta.saldo);
 		}
@@ -20,7 +20,7 @@ public class Referencias_Java {
 			System.out.println(primeiraconta.saldo);
 		}
 		
-		boolean valorParaSacar = primeiraconta.saca(20);
+		boolean valorParaSacar = primeiraconta.saca(20);               // Operando o saque e criando uma condição booleana.
 		if(valorParaSacar) {
 			System.out.println("Saque realizado com sucesso!.");
 			System.out.println(primeiraconta.saldo);
@@ -31,7 +31,7 @@ public class Referencias_Java {
 			System.out.println(primeiraconta.saldo);
 		}
 		
-		boolean valorParaTransferir = primeiraconta.transfere(100, segundaconta);
+		boolean valorParaTransferir = primeiraconta.transfere(100, segundaconta);   // Operando a transferencia, pegando a conta de destino para deposito.
 		if(valorParaTransferir) {
 			System.out.println("Transferencia realizada com sucesso!.");
 			System.out.println(primeiraconta.saldo);
